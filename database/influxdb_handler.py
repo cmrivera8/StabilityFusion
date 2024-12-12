2f4976af36a7eaeec6ce07467bccb16d2dc3df25
cmrivera8 <carlos.rivera8528@hotmail.com>
2024-12-13 00:29:49 +0100

    Update: Fetch data in 1-hours blocks to avoid overload. Add: tqdm progress bar to data fetching.

--- database/influxdb_handler.py
+++ database/influxdb_handler.py
@@ -2,9 +2,10 @@ import json
 import pandas as pd
 from pathlib import Path
 from types import SimpleNamespace
-from datetime import datetime
+from datetime import datetime, timedelta
 from zoneinfo import ZoneInfo
 from influxdb_client import InfluxDBClient
+from tqdm import tqdm
 
 def load_config(config_path):
     """Load configuration from a JSON file."""
@@ -41,28 +42,50 @@ class InfluxDBHandler:
             dt = datetime.fromisoformat(date_str).replace(tzinfo=ZoneInfo("Europe/Paris"))
             # From local datetime to UTC
             dt = dt.astimezone(ZoneInfo("UTC"))
-            # Apply format compatible with Flux
-            dt = dt.strftime("%Y-%m-%dT%H:%M:%S.%fZ")
             return dt
 
         start = string_to_date(start)
         stop = string_to_date(stop)
 
-        query = """
-        import "date"
-        from(bucket: "{db_bucket}")
-            |> range(start: {start}, stop: {stop})
-        """.format(db_bucket=self.bucket, start=start, stop=stop)
+        # Divide request in 1h blocks
+        block_duration = timedelta(hours=1)
+        total_duration = stop-start
+        use_progress = total_duration > block_duration
+        num_blocks = (total_duration//block_duration)+1
+        current_start = start
 
-        fluxtable_json = self.flux_to_points_obj(self.query_api.query(query, org=self.org))
+        iterator = tqdm(range(num_blocks), desc="Fetching data") if use_progress else range(num_blocks)
 
-        self.db_df = pd.DataFrame([vars(row) for row in fluxtable_json])
+        df_list = []
 
-        # Convert _time column to datetime in UTC
-        self.db_df["_time"] = pd.to_datetime(self.db_df["_time"], utc=True)
+        for _ in iterator:
+            current_stop = min(current_start + block_duration, stop)
 
-        # Convert _time column to Europe/Paris timezone
-        self.db_df["_time"] = self.db_df["_time"].dt.tz_convert("Europe/Paris")
+            start_str = current_start.strftime("%Y-%m-%dT%H:%M:%S.%fZ")
+            stop_str = current_stop.strftime("%Y-%m-%dT%H:%M:%S.%fZ")
+
+            query = """
+            import "date"
+            from(bucket: "{db_bucket}")
+                |> range(start: {start}, stop: {stop})
+            """.format(db_bucket=self.bucket, start=start_str, stop=stop_str)
+
+            fluxtable_json = self.flux_to_points_obj(self.query_api.query(query, org=self.org))
+
+            block_df = pd.DataFrame([vars(row) for row in fluxtable_json])
+
+            # Convert _time column to datetime in UTC
+            block_df["_time"] = pd.to_datetime(block_df["_time"], utc=True)
+
+            # Convert _time column to Europe/Paris timezone
+            block_df["_time"] = block_df["_time"].dt.tz_convert("Europe/Paris")
+
+            df_list.append(block_df)
+
+            current_start = current_stop
+
+        # Combine all chunks into the final DataFrame
+        self.db_df = pd.concat(df_list, ignore_index=True)
 
         # self.db_df.to_pickle("test.pkl") # Save
         # self.db_df = pd.read_pickle("test.pkl") # Load
